c6b1f4773d028b8531f903e556641e2e6dba0f92
mas123shaik <masthan.shaik@gmail.com>
2025-02-26 14:30:09 +0530

    terraform changes

--- catalogue.tf
+++ catalogue.tf
@@ -1,15 +1,15 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0f9f0f43cbf1baaf8"]
 
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
 }
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "catalogue" {
   zone_id = "Z02101962RY3FU3U9KSR5"
-  name    = "frontend-dev"
+  name    = "catalogue-dev"
   type    = "A"
   ttl     = 10
   records = [aws_instance.frontend.private_ip]
