eabb2ff6e40527f33408129a3f65ea54bcd3676a
mas123shaik <masthan.shaik@gmail.com>
2025-02-26 16:16:17 +0530

    terraform changes

--- frontend.tf
+++ frontend.tf
@@ -36,7 +36,7 @@ resource "null_resource" "frontend" {
     }
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/mas123shaik/mas_roboshop_ansible roboshop.yml -e component_name=frontend -e env-dev",
+      "ansible-pull -i localhost, -U https://github.com/mas123shaik/mas_roboshop_ansible roboshop.yml -e component_name=frontend -e env=dev",
     ]
   }
 }
