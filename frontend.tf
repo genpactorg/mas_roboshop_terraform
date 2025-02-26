80f489104d188743cd537372df5002f3e3fed61a
mas123shaik <masthan.shaik@gmail.com>
2025-02-26 13:43:18 +0530

    terraform changes

--- frontend.tf
+++ frontend.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0f9f0f43cbf1baaf8"]
 
   tags = {
     Name = "frontend"
