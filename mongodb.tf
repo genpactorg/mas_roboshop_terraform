ab872db63695fb12553ba209b4ad56f3e9d28063
mas123shaik <masthan.shaik@gmail.com>
2025-02-26 15:03:40 +0530

    terraform changes

--- mongodb.tf
+++ mongodb.tf
@@ -6,6 +6,19 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/mas123shaik/mas_roboshop_ansible roboshop.yml -e component_name=mongodb -e env=dev",
+    ]
+  }
 }
 resource "aws_route53_record" "mongodb" {
   zone_id = "Z02101962RY3FU3U9KSR5"
