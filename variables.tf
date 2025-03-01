259b0eff852c5e43d3e6b5af5294573bbdfae6e6
mas123shaik <masthan.shaik@gmail.com>
2025-03-01 18:21:40 +0530

    terraform changes

--- variables.tf
+++ variables.tf
@@ -1,17 +1,20 @@
-variable "ami_id" {
-  default = "ami-09c813fb71547fc4f"
-}
-variable "instance_type" {
-  default = "t3.small"
-}
 variable "vpc_security_group_ids" {
   default = ["sg-0740e8c441e48cb53"]
 }
 variable "instances" {
   default = {
-    catalogue = null
-    frontend = null
-    mongodb = null
+    catalogue = {
+      ami_id = "ami-09c813fb71547fc4f"
+      instance_type = "t3.small"
+    }
+    frontend = {
+      ami_id = "ami-09c813fb71547fc4f"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+      ami_id = "ami-09c813fb71547fc4f"
+      instance_type = "t3.small"
+    }
   }
 }
 variable "zone_id" {
