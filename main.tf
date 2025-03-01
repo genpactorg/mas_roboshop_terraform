84aedabe24e498bba7500d62fb41199c424af290
mas123shaik <masthan.shaik@gmail.com>
2025-03-01 18:15:56 +0530

    terraform changes

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
-    Name = var.instances[each.key]
+    Name = each.key
   }
 }
 resource "aws_route53_record" "record" {
   for_each = var.instances
   zone_id = var.zone_id
-  name    = "${var.instances[each.key]}-${var.env}"
+  name    = "${each.key}-${var.env}"
   type    = "A"
   ttl     = 10
   records = [aws_instance.instance[each.key].private_ip]
