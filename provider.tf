69cbf6e53bc755fa4543652644e56612d6ebb477
mas123shaik <masthan.shaik@gmail.com>
2025-04-14 19:02:15 +0530

    github action tool changes

--- provider.tf
+++ provider.tf
@@ -5,3 +5,9 @@ provider "vault" {
   address = "http://vault-internal.masdevops.online:8200"
   token   = var.vault_token
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
